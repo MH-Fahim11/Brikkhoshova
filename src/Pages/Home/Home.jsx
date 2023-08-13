fc55e8da88396cea6a38d033b680675f754ad5fa
MH-Fahim11 <fmahmudulhasan@gmail.com>
2023-08-13 10:01:26 +0600

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../../Components/Navbar/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar>
+                
+            </Navbar>
+        </div>
+    );
+};
+
+export default Home;
